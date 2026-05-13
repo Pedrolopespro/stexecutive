c971c31ea36d557a49330ad314e1421988dc8796
Pedro Lopes <lopeshpl@gmail.com>
2026-05-13 19:05:39 -0300

    feat: Google Ads tag + site verification + botão frota + fix menu Início

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,6 +51,18 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className="h-full">
       <head>
+        {/* Google Ads */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18160577429" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-18160577429');
+            `,
+          }}
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
