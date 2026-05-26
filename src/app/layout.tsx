3fc9d2a82f4e2f804bca4702173584ba35cd1445
Pedro Lopes <lopeshpl@gmail.com>
2026-05-26 12:02:51 -0300

    feat: adiciona Google Tag Manager (GTM-5GVMZZQJ)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,6 +51,16 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className="h-full">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5GVMZZQJ');`,
+          }}
+        />
         {/* Google Ads */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18160577429" />
         <script
@@ -96,6 +106,15 @@ export default function RootLayout({
         />
       </head>
       <body className="min-h-full flex flex-col antialiased" suppressHydrationWarning>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5GVMZZQJ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
       </body>
     </html>
